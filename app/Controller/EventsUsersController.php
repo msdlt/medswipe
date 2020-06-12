8828741be2853fc0f27cd2facdedb7756c0cce15
theotherdy <damion@allotmentor.com>
2020-06-12 15:10:49 +0100

    PhpSpreadhseet

--- app/Controller/EventsUsersController.php
+++ app/Controller/EventsUsersController.php
@@ -23,6 +23,7 @@ function multidimensional_search($parents, $searched)
 //App::import('Vendor', 'Spreadsheet', array('file' => 'PhpOffice'.DS.'PhpSpreadsheet'.DS.'Spreadsheet'.DS.'Spreadsheet.php'));
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 		
 class EventsUsersController extends AppController {
 
@@ -473,7 +474,45 @@ class EventsUsersController extends AppController {
 				$this->set('course_events', $course_events);
 				//instead of rendering a .ctp file, let's just write out directly
 				$spreadsheet = new Spreadsheet();
-				
+
+				$course_event_ids = array();
+
+				// define table cells 
+				$table = array( 
+					array('label' => __('Last name'), 'width' => 'auto', 'filter' => true), 
+					array('label' => __('First name'), 'width' => 'auto'), 
+					array('label' => __('No of Events'), 'width' => 'auto', 'filter' => true), 
+				);
+				foreach($course_events as $course_event){
+					array_push($table, array('label' => __($course_event['Event']['name']), 'width' => 'auto', 'filter' => true));
+					array_push($course_event_ids,'event_'.$course_event['Event']['id']);
+					}
+				////debug($table);	
+
+				// heading 
+				$spreadsheet->addTableHeader($table, array('name' => 'Cambria', 'bold' => true)); 
+
+				// data 
+				foreach ($course_users['rows'] as $course_user) { 
+					//debug($course_user);
+					$row_array = array($course_user['last_name'],$course_user['first_name'],$course_user['no_of_events']);
+					for ($i=0;$i<count($course_event_ids);$i++){
+						if(isset($course_user[$course_event_ids[$i]])){
+							array_push($row_array,$course_user[$course_event_ids[$i]]);
+							}
+						else{
+							array_push($row_array,'n/a');
+							}
+						}
+					$spreadsheet->addTableRow($row_array); 
+				}
+
+				$spreadsheet->addTableFooter();
+
+				//debug($this->PhpExcel)
+				$writer = new Xlsx($spreadsheet);
+				$writer ->save(WWW_ROOT . ‘/files/ example.xlsx’);
+							
 
 				//http://abakalidis.blogspot.com/2018/08/how-to-use-latest-phpofficespreadsheet.html
 
